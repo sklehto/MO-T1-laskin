f0df94aa770b930003ccc91a6ef1622b9b3ab83e
sklehto <susanna.lehto2@myy.haaga-helia.fi>
2022-08-28 23:33:20 +0300

    Tehtävä valmis.

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState(null);
+  const [number2, setNumber2] = useState(null);
+  const [result, setResult] = useState('');
+
+  const plusButtonPressed = () => {
+    setResult( Number(number1) + Number(number2) );
+  };
+
+  const minusButtonPressed = () => {
+    setResult( Number(number1) - Number(number2) );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        onChangeText={number => setNumber1(number)}
+        value={number1}
+      />
+      <TextInput
+        style={{width: 200, borderColor: 'grey', borderWidth: 1}}
+        keyboardType="numeric"
+        onChangeText={number => setNumber2(number)}
+        value={number2}
+      />
+      <View style={styles.button}>
+        <Button onPress={plusButtonPressed} title="+" />
+        <Button onPress={minusButtonPressed} title="-" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +46,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    borderColor: 'grey',
+    borderWidth: 1,
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: 90,
+    paddingTop: 25,
   },
 });
